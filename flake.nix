1abf87b3db04f52b189f5e1c56c80f5d8d7703d9
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-07-17 15:37:04 -0600

    Remove follow

--- flake.nix
+++ flake.nix
@@ -3,56 +3,50 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nvf = {
-      url = "github:notashelf/nvf";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nvf.url = "github:notashelf/nvf";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nvf,
-      ...
-    }@inputs:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
+  outputs = {
+    self,
+    nixpkgs,
+    nvf,
+    ...
+  } @ inputs: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
 
-      forEachSystem = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in
-    {
-      packages = forEachSystem (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          nvimConfiguration = nvf.lib.neovimConfiguration {
-            inherit pkgs;
-            modules = [
-              ./modules
-            ];
-          };
-        in
-        {
-          default = nvimConfiguration.neovim.overrideAttrs (old: {
-            meta = old.meta // {
+    forEachSystem = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in {
+    packages = forEachSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nvimConfiguration = nvf.lib.neovimConfiguration {
+          inherit pkgs;
+          modules = [
+            ./modules
+          ];
+        };
+      in {
+        default = nvimConfiguration.neovim.overrideAttrs (old: {
+          meta =
+            old.meta
+            // {
               license = pkgs.lib.licenses.mit;
               platforms = systems;
             };
-          });
-        }
-      );
+        });
+      }
+    );
 
-      nixosModules.default = { pkgs, ... }: {
-        programs.neovim.package = self.packages.${pkgs.system}.default;
-      };
-
-      homeManagerModules.default = { pkgs, ... }: {
-        programs.neovim.package = self.packages.${pkgs.system}.default;
-      };
+    nixosModules.default = {pkgs, ...}: {
+      programs.neovim.package = self.packages.${pkgs.system}.default;
     };
+
+    homeManagerModules.default = {pkgs, ...}: {
+      programs.neovim.package = self.packages.${pkgs.system}.default;
+    };
+  };
 }
